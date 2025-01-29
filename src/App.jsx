0f5f4bc150e3238ad761397e07e085da8edf696b
Vladyslava Korovaina <vladyslava.korovainaya@gmail.com>
2025-01-29 12:56:45 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,15 @@ import moviesFromServer from './api/movies.json';
 export const App = () => {
   const [query, setQuery] = useState('');
 
-  const visibleMovies = moviesFromServer.filter(movie => {
-    const normalizedQuery = query.trim().toLowerCase();
+  const visibleMovies = moviesFromServer.filter(
+    ({ title, description }) =>
+      `${title} ${description}`
+        .toLowerCase()
+        .includes(query.trim().toLowerCase()),
+    // eslint-disable-next-line function-paren-newline
+  );
 
-    return (
-      movie.title.toLowerCase().includes(normalizedQuery) ||
-      movie.description.toLowerCase().includes(normalizedQuery)
-    );
-  });
-
+  // eslint-disable-next-line no-unused-vars
   const handleInputChange = event => {
     setQuery(event.target.value);
   };
@@ -26,10 +26,10 @@ export const App = () => {
       <div className="filter">
         <input
           type="text"
-          className="filter__input"
-          placeholder="Search movies..."
+          id="search-query"
           value={query}
-          onChange={handleInputChange}
+          onChange={e => setQuery(e.target.value)}
+          placeholder="Search..."
         />
       </div>
 
